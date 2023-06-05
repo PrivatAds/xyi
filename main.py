4078d83e4e27a66087135725388ccf3f25ab35d6
PrivatAds <PrivatAds@proton.me>
2023-06-05 23:00:32 +0300

    3311122

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ async def not_found_exception_handler(request: Request, exc: HTTPException):
 
 @app.get("/")
 async def index(request:Request, fbclid: str|None=None):
+    logger.warning(fbclid)
     if active:
         if fbclid:
             logger.debug(f"{request.url}: \n Visited! {request.client}.")
